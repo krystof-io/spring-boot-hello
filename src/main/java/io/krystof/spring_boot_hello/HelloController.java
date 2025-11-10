bad57777a25d0b9097302541962e74db1f6d0dac
erkrystof <15527821+erkrystof@users.noreply.github.com>
2025-11-10 16:52:47 -0600

    Remove unused IOException import
    Removed unused import for IOException.

--- src/main/java/io/krystof/spring_boot_hello/HelloController.java
+++ src/main/java/io/krystof/spring_boot_hello/HelloController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 public class HelloController {
 
